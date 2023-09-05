7ab931b0d5b7e5554bf2b1152d670eb6b968fdee
lemonorangeapple <107529938+lemonorangeapple@users.noreply.github.com>
2023-09-05 18:24:53 +0800

    Update conf.js

--- conf.js
+++ conf.js
@@ -12,60 +12,18 @@ jsproxy_config({
 
   // 节点配置
   node_map: {
-    'demo-hk': {
-      label: '演示服务-香港节点',
-      lines: {
-        // 主机:权重
-        'node-aliyun-hk-1.etherdream.com:8443': 1,
-        'node-aliyun-hk-2.etherdream.com:8443': 2,
-      }
-    },
-    'demo-sg': {
-      label: '演示服务-新加坡节点',
-      lines: {
-        'node-aliyun-sg.etherdream.com:8443': 1,
-      },
-    },
     'mysite': {
       label: '当前站点',
       lines: {
         [location.host]: 1,
       }
-    },
-    // 该节点用于加载大体积的静态资源
-    'cfworker': {
-      label: '',
-      hidden: true,
-      lines: {
-        // 收费版（高权重）
-        //'node-cfworker-2.etherdream.com': 4,
-
-        // 免费版（低权重，分摊一些成本）
-        // 每个账号每天 10 万次免费请求，但有频率限制
-        //'b.007.workers.dev': 1,
-        //'b.hehe.workers.dev': 1,
-        //'b.lulu.workers.dev': 1,
-        //'b.jsproxy.workers.dev': 1,
-      }
     }
   },
 
-  /**
-   * 默认节点
-   */
   node_default: 'mysite',
   // node_default: /jsproxy-demo\.\w+$/.test(location.host) ? 'demo-hk' : 'mysite',
 
-  /**
-   * 加速节点
-   */
-  node_acc: 'cfworker',
-
-  /**
-   * 静态资源 CDN 地址
-   * 用于加速 `assets` 目录中的资源访问
-   */
-  // assets_cdn: 'https://cdn.jsdelivr.net/gh/zjcqoo/zjcqoo.github.io@master/assets/',
+  node_acc: 'mysite',
 
   // 本地测试时打开，否则访问的是线上的
   assets_cdn: 'assets/',
@@ -85,14 +43,5 @@ jsproxy_config({
    * URL 自定义处理（设计中）
    */
   url_handler: {
-    'https://www.baidu.com/img/baidu_resultlogo@2.png': {
-      replace: 'https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_92x30dp.png'
-    },
-    'https://www.pornhub.com/': {
-      redir: 'https://php.net/'
-    },
-    'http://haha.com/': {
-      content: 'Hello World'
-    },
   }
 })
